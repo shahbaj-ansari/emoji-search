8a31fe17410cfe39e1219596408310f49761cfa2
Shahbaj Ansari <shahbaj.ansari.17@mountblue.tech>
2021-10-10 18:54:11 +0530

    fixed hovering bug by not using state and used css only to do it

--- src/components/EmojiList.js
+++ src/components/EmojiList.js
@@ -3,30 +3,18 @@ import "../css/EmojiList.css";
 import { v4 as uuidv4 } from 'uuid';
 
 class EmojiList extends React.Component {
-
-    state = {
-        Copystyle: "hide"
-    }
-
-    onHover = () =>{
-        this.setState({Copystyle : "copy-emoji"})      
-    }
-
-    onHoverOff = () =>{
-          this.setState({Copystyle : "hide"})      
-    }
   
     // onClicking in an emoji copy it
     emojiClicked= ()=> {
-        navigator.clipboard.writeText(this.props.emoji.symbol);
+       navigator.clipboard.writeText(this.props.emoji.symbol);
     }
 
     render () {
         return (
-            <div key={uuidv4()} className="emoji-row" onClick={this.emojiClicked} onMouseEnter={this.onHover} onMouseLeave={this.onHoverOff}>
+            <div key={uuidv4()} className="emoji-row" onClick={this.emojiClicked}>
                 <span className="symbol">{this.props.emoji.symbol}</span>
                 <span>{this.props.emoji.title}</span>
-                <span className={this.state.Copystyle}>Click to copy emoji</span>
+                <span className="copy-emoji">Click to copy emoji</span>
             </div>
         )
     }
